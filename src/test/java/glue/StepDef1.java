cfd1cdf47d782869e3a2765d05643d2b28157b97
krishnasai33 <saikrishna33@sai.gmail.com>
2023-01-29 17:17:58 +0530

    first

---
+++ src/test/java/glue/StepDef1.java
@@ -0,0 +1,10 @@
+package glue;
+
+public class StepDef1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/utilities/Screens.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class Screens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
